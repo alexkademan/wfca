76ac78212d01f267cf7010d2a5978e8995539e00
Alex Kademan <alexkademan@gmail.com>
2016-10-13 13:28:22 -0500

    parallax effect seems to be working

--- _assets/js/views/parallax_view.js
+++ _assets/js/views/parallax_view.js
@@ -10,30 +10,42 @@ module.exports = Backbone.View.extend({
   initialize: function() {
 
     if(this.$el.length === 1){
-      console.log("this is the parallax javascript controller.");
+
+      // this is a static var, so log it now, and use it in the
+      // parallax method as needed.
+      this.plxBackground = this.$("#js-parallax-background");
+
       this.parallax();
-      this.model.on({
-        "change:vScrollPosition change:windowWidth": this.parallax
-      }, this);
+      this.model.on(
+        {"change:vScrollPosition change:windowWidth": this.parallax},
+        this
+      );
     };
 
   },
 
 
   parallax: function() {
-    var plxBackground = this.$("#js-parallax-background");
 
-    var plxWindowTopToPageTop = this.$el.offset().top;
-    var windowTopToPageTop = $(window).scrollTop();
-    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
-    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
-    var windowInnerHeight = window.innerHeight;
-    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
-    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-    // var plxSpeed = 0.35;
-    var plxSpeed = 0.5;
+    if( this.model.get("palmSize")  == false ){
+      // only process this thing if we're not on a phone sized screen:
+      var plxWindowTopToPageTop = this.$el.offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+      var plxBackgroundTopToPageTop = $(this.plxBackground).offset().top;
+      var windowInnerHeight = window.innerHeight;
+      var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+      var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+      var plxSpeed = 0.5;
 
-    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+      this.plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+
+
+    } else {
+      // we're smaller than the smallest of breakpoints...
+      // so disable the parallax effect:
+      this.plxBackground.css("top", "0px");
+    }
   },
 
 });
