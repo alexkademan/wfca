773625bb34b4fb62b9d40098a5394a0c65111fe6
Alex Kademan <alexkademan@gmail.com>
2017-05-22 17:34:31 -0500

    some small linting

--- _assets/js/views/window_stats_view.js
+++ _assets/js/views/window_stats_view.js
@@ -6,55 +6,57 @@ var $ = require ('jquery');
 
 module.exports = Backbone.View.extend({
 
-  el: window,
+    el: window,
 
-  initialize: function() {
+    initialize: function() {
 
-    app.windowStatus.set({
-      'vScrollLastPosition': $(document).scrollTop()
-    });
+        app.windowStatus.set({
+            'vScrollLastPosition': $(document).scrollTop()
+        });
 
-    this.updatePageSize();
-    this.$el.on('resize', this.updatePageSize);
-    this.$el.on('scroll', this.updatePageSize);
-
-
-  },
-
-  updatePageSize: function() {
-    var windowWidth = $(window).width(), //retrieve current window width
-        windowHeight = $(window).height(), //retrieve current window height
-        documentWidth = $(document).width(), //retrieve current document width
-        documentHeight = $(document).height(), //retrieve current document height
-        vScrollPosition = $(document).scrollTop(); //retrieve the document scroll TOP position
-        // hScrollPosition = $(document).scrollLeft();
+        this.updatePageSize();
+        this.$el.on('resize', this.updatePageSize);
+        this.$el.on('scroll', this.updatePageSize);
 
 
-    app.windowStatus.set({
-      windowWidth: windowWidth,
-      windowHeight: windowHeight,
-      documentWidth: documentWidth,
-      documentHeight: documentHeight,
-      vScrollPosition: vScrollPosition
-    });
+    },
 
-    if (windowWidth > app.windowStatus.get('palmWidth')){
-      app.windowStatus.set({'palmSize': false});
-    } else {
-      app.windowStatus.set({'palmSize': true});
+    updatePageSize: function() {
+        var windowWidth = $(window).width(),
+            windowHeight = $(window).height(),
+            documentWidth = $(document).width(),
+            documentHeight = $(document).height(),
+            vScrollPosition = $(document).scrollTop();
+            // hScrollPosition = $(document).scrollLeft();
+
+        app.windowStatus.set({
+            windowWidth: windowWidth,
+            windowHeight: windowHeight,
+            documentWidth: documentWidth,
+            documentHeight: documentHeight,
+            vScrollPosition: vScrollPosition
+        });
+
+        if (windowWidth > app.windowStatus.get('palmWidth')){
+            app.windowStatus.set({'palmSize': false});
+        } else {
+            app.windowStatus.set({'palmSize': true});
+        }
+
+        var dir;
+        if(vScrollPosition > app.windowStatus.get('vScrollLastPosition')) {
+            dir = 'down';
+        } else if (vScrollPosition < app.windowStatus.get('vScrollLastPosition')) {
+            dir = 'up';
+        }
+
+        // send to the model if it has changed from what the model shows:
+        if(dir && dir !== app.windowStatus.get('vScrollDirection')) {
+            app.windowStatus.set({ vScrollDirection: dir });
+        }
+
+        // done checking, so set last scroll position:
+        app.windowStatus.set({ vScrollLastPosition: vScrollPosition });
     }
-    var dir;
-    if(vScrollPosition > app.windowStatus.get('vScrollLastPosition')) {
-      dir = 'down';
-    } else if (vScrollPosition < app.windowStatus.get('vScrollLastPosition')) {
-      dir = 'up';
-    }
-    // send to the model if it has changed from what the model shows:
-    if(dir && dir !== app.windowStatus.get('vScrollDirection')) {
-      app.windowStatus.set({ vScrollDirection: dir });
-    }
-    // done checking, so set last scroll position:
-    app.windowStatus.set({ vScrollLastPosition: vScrollPosition });
-  }
 
 });
